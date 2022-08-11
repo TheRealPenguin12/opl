039d34aa5ca07fbf8be0bb21b63444bdb20e401f
desvasicek <84301435+desvasicek@users.noreply.github.com>
2022-08-11 09:37:30 -0700

    Add messages
    Add messages and make it print __all__ results :thumbsup:

--- find_string.py
+++ find_string.py
@@ -1,10 +1,17 @@
 import sys, os, re
 from colorama import Style
+print("Receiving data...")
 str_to_find = f".+/{re.escape(sys.argv[1])}"
 str_to_find_in = open("report.txt", "r").read()
+print("Data recieved.")
 print("Please wait...")
-search = re.search(f'{str_to_find}', str_to_find_in)
+search = re.findall(f'{str_to_find}', str_to_find_in)
+already_found = []
+print("Searching...\n")
 if search:
-    print(f"Found: {search.group(0).replace(str_to_find, f'{Style.BRIGHT}sys.argv[1]{Style.RESET_ALL}')}.")
+    for result in search:
+        if not result in already_found:
+            print(f"Found: {result.replace(str_to_find, f'{Style.BRIGHT}sys.argv[1]{Style.RESET_ALL}')}.")
+            already_found.append(result)
 else:
     print("Could not find.")
