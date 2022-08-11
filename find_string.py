ef12e46e9dc66f45391b5245384cac4adc187ef9
desvasicek <84301435+desvasicek@users.noreply.github.com>
2022-08-11 15:03:27 -0700

    Update

--- find_string.py
+++ find_string.py
@@ -4,14 +4,16 @@ print("Receiving data...")
 str_to_find = f".+/{re.escape(sys.argv[1])}"
 str_to_find_in = open("report.txt", "r").read()
 print("Data recieved.")
-print("Please wait...")
+print("Searching...\n")
 search = re.findall(f'{str_to_find}', str_to_find_in)
 already_found = []
-print("Searching...\n")
+counter = 0
 if search:
     for result in search:
         if not result in already_found:
-            print(f"Found: {result.replace(str_to_find, f'{Style.BRIGHT}sys.argv[1]{Style.RESET_ALL}')}.")
+            counter += 1
+            print(f"{counter}. Found: {result.replace(sys.argv[1], f'{Style.BRIGHT}{sys.argv[1]}{Style.RESET_ALL}')}.")
             already_found.append(result)
+        sys.stdout.flush()
 else:
     print("Could not find.")
